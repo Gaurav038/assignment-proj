1b32215aa3b4cd8efeb7c8ed55c45d9924750093
Gaurav Singh <gauravkarki038@gmail.com>
2023-06-24 16:51:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [filtered, setFiltered] = useState([])
+  const [searchName, setSearchName] = useState("");
+
+  
+  const handleSearch = async() => {
+
+    try {
+      const data = await fetch('https://reqres.in/api/users?page=2')
+      const result = await data.json()
+
+      const filteredData = result?.data.filter((student) =>
+        student.first_name.toLowerCase().includes(searchName.toLowerCase())
+      );
+
+      setFiltered(filteredData)
+    } catch (error) {
+      console.log(error);
+    }
+    
+  };
+
+  useEffect(() => {
+    handleSearch()
+  },[searchName])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='search-box'>
+        <input
+          type="text"
+          value={searchName}
+          placeholder='enter to search'
+          onChange={(e) => setSearchName(e.target.value)}
+        />
+      </div>
+
+      {
+        filtered && filtered.map((value) => {
+          return <div key={value.first_name}>
+            <div className='box-border'>
+              <span className='id-div'>{value.id}</span>
+              <img src={value.avatar} />
+            </div>
+              <div style={{fontSize: '20px'}} >{value.first_name}</div>
+          </div>
+        })
+      }
     </div>
   );
 }
